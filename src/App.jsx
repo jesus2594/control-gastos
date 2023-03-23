3109afce90c0fbfc7e61b87c4a5a0c96f2a3184e
jesus2594 <jesusdavid890@hotmail.com>
2023-03-22 22:10:44 -0500

    Estructura Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>
+      Control de Gastos
+    </h1>
+  )
+}
+
+export default App
