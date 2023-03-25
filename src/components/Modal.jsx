4c5d8863d4946b5673bfc12533e5b63827eaa771
jesus2594 <jesusdavid890@hotmail.com>
2023-03-25 00:18:08 -0500

    Se agrega la funcion de eliminar

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,12 +9,16 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar
   const [nombre, setNombre] = useState('')
   const [cantidad, setCantidad] = useState('')
   const [categoria, setCategoria] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [id, setId] = useState('')
 
   useEffect(() => {
     if(Object.keys(gastoEditar).length > 0) {
       setNombre(gastoEditar.nombre)
       setCantidad(gastoEditar.cantidad)
       setCategoria(gastoEditar.categoria)
+      setId(gastoEditar.id)
+      setFecha(gastoEditar.fecha)
     }
   }, [])
   
@@ -37,7 +41,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar
         }, 3000);
         return;
       }
-      guardarGasto({nombre, cantidad, categoria})
+      guardarGasto({nombre, cantidad, categoria, id, fecha})
     }
 
   return (
@@ -49,7 +53,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar
         <form 
             onSubmit={handleSubmit}
             className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
-          <legend>Nuevo Gasto</legend>
+          <legend>{gastoEditar.nombre ? "Editar Gasto" : "Nuevo Gasto"}</legend>
           {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
 
           <div className='campo'>
@@ -95,7 +99,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto, gastoEditar
             </select>
             
           </div>
-          <input type="submit" value="Añadir Gasto" />
+          <input type="submit" value={gastoEditar.nombre ? "Guardar Cambios" : "Añadir Gasto"} />
         </form>
     </div>
   )
